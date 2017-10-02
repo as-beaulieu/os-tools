a440480be1a9c9ee9bf748091186a1f8129d3224
as-beaulieu <as.beaulieu@gmail.com>
2017-10-02 10:21:25 -0600

    Created VS2017 Solution for C# os tools

---
+++ cSharp/TimeSolution/Time/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //To access arguments while compiling in VS17
+            //In Solution Explorer, right click project, click 'Properties'
+            //Click 'Debug'
+            //enter arguments, and Ctrl-S to save
+            //Return to program, in Debug Menu, select "Start without debugging"
+            if(args.Length > 0)
+            {
+                Console.WriteLine("Hello, " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+            
+            //DateTime package under the System Library
+            Console.WriteLine("Today is: " + DateTime.Now.DayOfWeek);
+            Console.WriteLine("And the time is: " + DateTime.Now.TimeOfDay);
+
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You should be well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
